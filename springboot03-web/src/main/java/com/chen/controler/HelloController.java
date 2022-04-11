1be36df8e7d0b5fdb8f1fc0e27719c9527357156
cyc1998cyc <3502104521@qq.com>
2022-04-11 18:15:25 +0800

    笔记本提交-2

--- springboot03-web/src/main/java/com/chen/controler/HelloController.java
+++ springboot03-web/src/main/java/com/chen/controler/HelloController.java
@@ -1,17 +1,24 @@
 package com.chen.controler;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 陈宇超
  * 17:46
  */
-@RestController
+//如果是返回静态资源只能使用Controller
+//这个需要模板引擎的支持
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "/index";
     }
 }
