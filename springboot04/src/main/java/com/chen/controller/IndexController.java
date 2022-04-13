6543b580f693ea08ad5e1edd5759812ba847ab66
cyc1998cyc <3502104521@qq.com>
2022-04-14 01:49:21 +0800

    台式机提交

---
+++ springboot04/src/main/java/com/chen/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.chen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping({"/","/index.html"})
+    public String Index(){
+        return "index";
+    }
+}
