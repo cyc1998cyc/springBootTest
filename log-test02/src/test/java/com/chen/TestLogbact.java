914d33fe69bbdc30cffa73610e7bdad49beaa461
cyc1998cyc <3502104521@qq.com>
2022-07-22 09:40:51 +0800

    笔记本提交

--- log-test02/src/test/java/com/chen/TestLogbact.java
+++ log-test02/src/test/java/com/chen/TestLogbact.java
@@ -6,16 +6,16 @@ import org.slf4j.LoggerFactory;
 
 public class TestLogbact {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestLogbact.class);
+
     @Test
-    public void testLogback1(){
+    public void testLogback1() {
         //打印日志信息
-        while (true){
-            LOGGER.error("error");
-            LOGGER.warn("warn");
-            LOGGER.info("info");
-            LOGGER.debug("debug");
-            LOGGER.trace("trace");
-        }
+        LOGGER.error("error");
+        LOGGER.warn("warn");
+        LOGGER.info("info");
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
+
 
     }
 }
